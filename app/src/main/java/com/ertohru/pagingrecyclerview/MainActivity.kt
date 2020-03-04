734dbdb8f91284f1c656350676b0a42c52b3334a
erthru <ersaka96@gmail.com>
2020-03-05 01:37:20 +0800

    add progress bar in recyclerview

--- app/src/main/java/com/ertohru/pagingrecyclerview/MainActivity.kt
+++ app/src/main/java/com/ertohru/pagingrecyclerview/MainActivity.kt
@@ -2,18 +2,18 @@ package com.ertohru.pagingrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
-import android.widget.Toast
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val data = ArrayList<String>()
+    private val data = ArrayList<String?>()
     private val peopleAdapter = PeopleAdapter(data)
-
     private var allDataLoaded = false
+    private var isLoadingNext = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,86 +21,83 @@ class MainActivity : AppCompatActivity() {
 
         setupRecyclerView()
         fillData()
-
     }
 
     private fun setupRecyclerView(){
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = peopleAdapter
 
-        // check if recyclerview scroll reach the bottom
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
-
             var directiorDown:Boolean = false
 
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 directiorDown = dy > 0
             }
 
-
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-
-                if (recyclerView.canScrollVertically(1).not()
-                    && newState == RecyclerView.SCROLL_STATE_IDLE
-                    && directiorDown) {
-
-                    Log.d("RECYCLER_VIEW","has reached the bottom")
-
-                    if(!allDataLoaded)
+                if (recyclerView.canScrollVertically(1).not() && newState == RecyclerView.SCROLL_STATE_IDLE && directiorDown) {
+                    if(!allDataLoaded && !isLoadingNext)
                         fillDataNext()
-                    else
-                        Toast.makeText(this@MainActivity, "All data loaded", Toast.LENGTH_SHORT).show()
-
-                }else{
-
                 }
-
             }
-
         })
     }
 
     private fun fillData(){
-        val data = ArrayList<String>()
-        data.add("A")
-        data.add("B")
-        data.add("C")
-        data.add("D")
-        data.add("E")
-        data.add("F")
-        data.add("G")
-        data.add("H")
-        data.add("I")
-        data.add("J")
-        data.add("K")
-        data.add("L")
-        data.add("M")
-        data.add("N")
-        data.add("O")
-        data.add("P")
-        data.add("Q")
+        peopleAdapter.showProgress()
 
-        this.data.addAll(data)
+        // simulate network delay
+        Handler().postDelayed({
+            peopleAdapter.removeProgress()
 
-        // make adapter check the new data or data has changed
-        peopleAdapter.notifyDataSetChanged()
+            val data = ArrayList<String>()
+            data.add("A")
+            data.add("B")
+            data.add("C")
+            data.add("D")
+            data.add("E")
+            data.add("F")
+            data.add("G")
+            data.add("H")
+            data.add("I")
+            data.add("J")
+            data.add("K")
+            data.add("L")
+            data.add("M")
+            data.add("N")
+            data.add("O")
+            data.add("P")
+            data.add("Q")
+
+            this.data.addAll(data)
+            peopleAdapter.notifyDataSetChanged()
+        }, 3000)
     }
 
     private fun fillDataNext(){
-        val data = ArrayList<String>()
-        data.add("R")
-        data.add("S")
-        data.add("T")
-        data.add("U")
-        data.add("V")
-        data.add("W")
-        data.add("X")
-        data.add("Y")
-        data.add("Z")
+        this.isLoadingNext = true
 
-        this.allDataLoaded = true
+        peopleAdapter.showProgress()
 
-        this.data.addAll(data)
-        peopleAdapter.notifyDataSetChanged()
+        // simulate network delay
+        Handler().postDelayed({
+            peopleAdapter.removeProgress()
+
+            val data = ArrayList<String>()
+            data.add("R")
+            data.add("S")
+            data.add("T")
+            data.add("U")
+            data.add("V")
+            data.add("W")
+            data.add("X")
+            data.add("Y")
+            data.add("Z")
+
+            this.isLoadingNext = false
+            this.allDataLoaded = true
+            this.data.addAll(data)
+            peopleAdapter.notifyDataSetChanged()
+        }, 3000)
     }
 }
--- app/src/main/java/com/ertohru/pagingrecyclerview/PeopleAdapter.kt
+++ app/src/main/java/com/ertohru/pagingrecyclerview/PeopleAdapter.kt
@@ -6,24 +6,45 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_people.view.*
 
-class PeopleAdapter(val data: ArrayList<String>?): RecyclerView.Adapter<PeopleAdapter.ViewHolder>(){
+class PeopleAdapter(private val data: ArrayList<String?>?): RecyclerView.Adapter<PeopleAdapter.ViewHolder>(){
+    private val TYPE_VIEW = 0
+    private val TYPE_PROGRESS = 1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_people, parent, false))
+        return when(viewType){
+           TYPE_VIEW -> ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_people, parent, false))
+            else -> ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_progress, parent, false))
+        }
     }
 
     override fun getItemCount(): Int = data?.size ?: 0
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.build(data?.get(position)!!)
+        if(getItemViewType(position) != TYPE_PROGRESS){
+            holder.build(data?.get(position)!!)
+        }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return when(data?.get(position)){
+            null -> TYPE_PROGRESS
+            else -> TYPE_VIEW
+        }
+    }
+
+    fun showProgress(){
+        data?.add(null)
+        notifyItemInserted(data?.size!! - 1)
+    }
+
+    fun removeProgress(){
+        data?.removeAt(data.size - 1)
+        notifyItemRemoved(data?.size!!)
+    }
 
     class ViewHolder(val v: View): RecyclerView.ViewHolder(v){
-
         fun build(dataSet:String){
             v.textView.text = dataSet
         }
-
     }
 }
